8d004ef56eee6ea04d5d789f73fefe0a7d550c2d
alfandi <fandidixpc1@gmail.com>
2023-05-15 16:02:52 +0700

    Add project files.

---
+++ smbstatic2/smbstatic2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setId();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; /*definisi Function*/  }
+	static int getNim() { return nim; /*definisi function*/  }
+
+	mahasiswa(string pnama) :nama(pnama) { setId(); }
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setId(); {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Sri Dadi");
+	mahasiswa mhs2("Budi Jatmiko");
+	mahasiswa::setNim(9); //mengakses nim melalui static member fuction "set nim'
+	mahasiswa mhs3("Andi Janu");
+	mahasiswa mhs4("Joko Wahono");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "akses dari luar object = " << mahasiswa::getNim() << endl; //mengakses nim dari luar object
+}
